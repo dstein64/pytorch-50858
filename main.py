9d7667d134d3397a6c7917378da43132ee769b5a
Daniel Steinberg <dstein64@users.noreply.github.com>
2021-01-20 19:17:58 -0500

    Add print statements.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision.models as models
 
+print('start')
 resnet18 = models.resnet18(pretrained=True)
 loss = torch.tensor([1.0], requires_grad=True)
 loss.backward()
+print('end')
